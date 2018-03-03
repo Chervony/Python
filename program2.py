5b744fb9c3bf7cf58e0a58ac590fcf46d165170a
Chervony <egorhe451@gmail.com>
2018-03-03 17:21:51 +0600

    2  программы на опеределение дня и возраста

---
+++ program2.py
@@ -0,0 +1,15 @@
+time = int(input("Введите время "))
+if time >= 6 and time <= 11:
+    print("Утро")
+elif time == 12:
+    print("Полдень")
+elif time >= 13 and time <=18:
+    print("Обед")
+elif time >= 19 and time <= 22:
+    print("Вечер")
+elif time == 24:
+    print("Полночь")
+elif time >=1 and time <= 5:
+    print("Ночь")
+elif time <=0 or time >= 25:
+    print ("такого времени не существует")
